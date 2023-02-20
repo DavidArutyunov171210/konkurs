8fb189148fd4b397a690e476d73ecc587a7c2138
DavidArutyunov171210 <125858677+DavidArutyunov171210@users.noreply.github.com>
2023-02-20 10:39:10 +0300

    Update room1.py

--- room1.py
+++ room1.py
@@ -1,1 +1,45 @@
 from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+
+w = 920
+h = 600
+
+def door3():
+    win3=Tk()
+    win3.mainloop()
+
+
+
+
+win = Tk()
+win.resizable(False,False)
+win.title('Начало КвЕЕста')
+
+
+
+
+# настроили фон окна
+canvas = Canvas(win, width=w, height=h)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open('fone.png'))
+canvas.create_image(w // 2, h // 2, image=img)
+
+
+
+
+
+# поставили три кнопки в комнате
+door1 = ImageTk.PhotoImage(Image.open('door1.png'))
+btndoor1 = Button(image=door1)
+btndoor1.place(x=404, y=18)
+
+door2 = ImageTk.PhotoImage(Image.open('door2.png'))
+btndoor2 = Button(image=door2)
+btndoor2.place(x=256, y=318)
+
+door3 = ImageTk.PhotoImage(Image.open('door3.png'))
+btndoor3 = Button(image=door3,command=door3)
+btndoor3.place(x=555, y=293)
+
+win.mainloop()
