30e49897facacab99614c379ff9148e0fe2730d5
DavidArutyunov171210 <125858677+DavidArutyunov171210@users.noreply.github.com>
2023-02-22 12:39:38 +0300

    Update windiws_quest.py

--- windiws_quest.py
+++ windiws_quest.py
@@ -1,42 +1,73 @@
 from tkinter import *
 
+counter = 0
+
 
 def showraven():
-    room1 = Toplevel()
+    def button1def():
+        global counter
+        counter += 1
+        button1.place_forget()
+        ifcounter()
+
+    def button2def():
+        global counter
+        counter += 1
+        button2.place_forget()
+        ifcounter()
+
+    def button3def():
+        global counter
+        counter += 1
+        button3.place_forget()
+        ifcounter()
+
+    def ifcounter():
+        if counter == 0:
+            counter == 0
+        elif counter == 3:
+            
+            next = Button(room1, text='Дальше', font='Arial 50')
+            next.place(x=700, y=600)
+
+    def dalshedef():
+        end1 = Tk()
+        end1.mainloop()
+
+    room1 = Tk()
     room1.resizable(False, False)
     room1.geometry('1080x720+100+100')
 
     room1['bg'] = 'gray22'
 
     text1 = Label(
+        room1,
         text='Выберете все правильные математические равенства.\n(Если равенство верно , то при нажатии кнопка пропадет)\n(Правельных равенств ровно 3) ',
         fg='white', background='gray22', font='Arial 30')
     text1.pack()
 
     # Buttons
-    button1 = Button(room1, text='2+2=4', fg='black')
-    button1.place(x=200, y=100)
+    button1 = Button(room1, text='2+2=4', fg='black', font='Arial 20', command=button1def)
+    button1.place(x=300, y=200)
 
-    button2 = Button(room1, text='6*6=36')
-    button2.place()
+    button2 = Button(room1, text='6*6=36', fg='black', font='Arial 20', command=button2def)
+    button2.place(x=300, y=400)
 
-    button3 = Button(room1, text='4^2=16')
-    button3.place()
+    button3 = Button(room1, text='4^2=16', fg='black', font='Arial 20', command=button3def)
+    button3.place(x=500, y=200)
     # -----------------------------------------------------------------
-    button3 = Button(room1, text='99+8=108')
-    button3.place()
+    button4 = Button(room1, text='99+8=108', fg='black', font='Arial 20')
+    button4.place(x=500, y=400)
 
-    button4 = Button(room1, text='3+3=7')
-    button4.place()
+    button5 = Button(room1, text='3+3=7', fg='black', font='Arial 20')
+    button5.place(x=700, y=200)
 
-    button5 = Button(room1, text='3+3>3*2')
-    button5.place()
-
-    button6 = Button(room1, text='7*8=65')
-    button6.pack()
+    button6 = Button(room1, text='3+3>3*2', fg='black', font='Arial 20')
+    button6.place(x=700, y=400)
 
     room1.mainloop()
 
 
 if __name__ == '__main__':
     showraven()
+
